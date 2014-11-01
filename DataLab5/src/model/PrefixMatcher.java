6036ee1a11c5b3fe8414c074f6d5383a5db4954e
geiselc <geiselc@mail.gvsu.edu>
2014-11-01 12:54:52 -0400

    Simple .gz file reading
    Reads in .gz files. Prints out each line in the file. Doesn't do much
    else yet.
    
    Added a test.txt.gz file that is much shorter (the file he gave us has
    7, 411, 442 lines to process (which takes about 7 minutes for my desktop
    to process)). My test file only has 60 lines, which should make doing
    basic debugging a little easier.

--- DataLab5/src/model/PrefixMatcher.java
+++ DataLab5/src/model/PrefixMatcher.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.zip.*;
+
 public class PrefixMatcher {
 	private String file1;
 	private String file2;
@@ -8,9 +15,39 @@ public class PrefixMatcher {
 	public static void main(String[] args) {
 		new PrefixMatcher(args[0], args[1], args[2]);
 	}
+	
 	public PrefixMatcher(String file1, String file2, String length) {
 		this.file1 = file2;
 		this.file2 = file2;
 		prefixLength = Integer.parseInt(length);
+		
+		String line;
+		try{
+			GZIPInputStream gZip = new GZIPInputStream(new FileInputStream(file1));
+			BufferedReader br = new BufferedReader(new InputStreamReader(gZip));
+			while((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			
+			br.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+//		String line;
+//		String[] parts = {};
+//		int i = 0;
+//		try {
+//			BufferedReader br = new BufferedReader(new FileReader(file1));
+//			while ((line = br.readLine()) != null) {
+//				line = br.readLine();
+//				parts[i] = line;
+//				System.out.println(parts[i]);
+//				i++;
+//			}
+//			line = br.readLine();
+//			br.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 }
